b7ca127187145ae20687cfdae662bcc4004efd56
dianasnfonseca <dianasnfonseca@gmail.com>
2024-03-18 18:37:47 +0100

    test errors client

--- client/client.py
+++ client/client.py
@@ -1,3 +1,4 @@
+# client.py
 import socket
 import configparser
 from serialization import serialize_data
@@ -17,71 +18,50 @@ def send_data(data, serialization_format, file_path=None, encrypt_file=False):
         None
     """
     try:
-        # Load client configuration
         config = configparser.ConfigParser()
         config.read('config/client_config.ini')
 
-        # Extract server IP and port from configuration
         server_ip = '127.0.0.1'
         server_port_string = config['SERVER']['PORT'].strip()
         server_port = int(''.join(filter(str.isdigit, server_port_string)))
 
-        # Create socket object
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-            # Set SO_REUSEADDR option
             client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            
-            # Connect to server
             client_socket.connect((server_ip, server_port))
-            print("Connected to server.")
 
-            # Serialize data based on chosen format
             serialized_data = serialize_data(data, serialization_format)
-            
-            # Print the serialization format and data
-            print(f"Data serialized to {serialization_format.upper()} format:", serialized_data)
 
-            # Combine serialization format and length of serialized data
             message = f"{serialization_format} {len(serialized_data)}"
             client_socket.sendall(message.encode())
-            print("Serialization format and length sent to server:", message)
 
-            # Send serialized data to server
             client_socket.sendall(serialized_data)
-            print("Serialized data sent to server.")
 
-            # Load encryption key from file
-            encryption_key_path = 'config/encryption_key.txt'
+            encryption_key_path = config['ENCRYPTION']['KEY_PATH']
             with open(encryption_key_path, 'rb') as key_file:
                 encryption_key = key_file.read()
 
-            # Send file data to server if provided
             if file_path:
                 with open(file_path, 'rb') as file:
                     file_data = file.read()
                     if encrypt_file:
-                        # Encrypt file data using the encryption key
                         encrypted_file_data = encrypt_data(file_data, encryption_key)
                         if encrypted_file_data:
                             file_length = len(encrypted_file_data)
-                            client_socket.sendall(str(file_length).encode())  # Send file data length
-                            # Wait for acknowledgment from the server
+                            client_socket.sendall(str(file_length).encode())
                             client_socket.recv(1024)
                             client_socket.sendall(encrypted_file_data)
-                            print("Encrypted file data sent to server.")
                     else:
                         file_length = len(file_data)
-                        client_socket.sendall(str(file_length).encode())  # Send file data length
-                        # Wait for acknowledgment from the server
+                        client_socket.sendall(str(file_length).encode())
                         client_socket.recv(1024)
                         client_socket.sendall(file_data)
-                        print("File data sent to server.")
 
     except Exception as e:
         print(f"Error occurred: {e}")
     finally:
-        # Close the socket connection
-        client_socket.close()
+        if client_socket:
+            # Close the socket connection
+            client_socket.close()
 
 if __name__ == "__main__":
     try:
